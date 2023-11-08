a70f5560dd891dfa400558382a803937aa1fe1b7
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2023-11-08 19:14:44 +0700

    Mamaa, just killed a mannn!11

---
+++ src/LSTM/haha.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+
+
+def plottraintest(train, test):
+    fig =plt.figure(figsize=(12,8))
+    ax=fig.add_subplot(2,2,1)
+    ax.plot(train, c="blue")
+    ax.set_title("train")
+    ax=fig.add_subplot(2,2,2)
+    ax.plot(test, c="orange")
+    ax.set_title("test")
+    ax=fig.add_subplot(2,1,2)
+    temp=np.array(list(map(list, zip(train.flatten(), range(1,len(train)+1)))))
+    ax.plot(temp[:,1], temp[:,0], c="blue", label="train")
+    temp=np.array(list(map(list, zip(test.flatten(), range(len(train)+1,len(train)+len(test)+1)))))
+    ax.plot(temp[:,1], temp[:,0], c="orange", label="test")
+    ax.legend(loc="lower right")
+    ax.set_title("Train & test")
+    
+    
+def lag(dataset, t=1):
+    '''Default t=1'''
+    dataX, dataY = [], []
+    for i in range(len(dataset)-t-1):
+        a = dataset[i:(i+t), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + t, 0])
+    return np.array(dataX), np.array(dataY)
+
+def plot_predicted(df, trainPredict, testPredict, t):
+    # shift train predictions for plotting
+    trainPredictPlot = np.empty_like(df)
+    trainPredictPlot[:, :] = np.nan
+    trainPredictPlot[t:len(trainPredict)+t, :] = trainPredict
+
+    # shift test predictions for plotting
+    testPredictPlot = np.empty_like(df)
+    testPredictPlot[:, :] = np.nan
+    testPredictPlot[len(trainPredict)+(t*2)+1:len(df)-1, :] = testPredict
+
+    # plot baseline and predictions
+    fig=plt.figure(figsize=(12,8))
+    ax=fig.add_subplot(1,1,1)
+    ax.grid("on", alpha=0.4)
+    ax.plot(df, alpha=0.8, label="actual")
+    ax.plot(trainPredictPlot, label="predicted train")
+    ax.plot(testPredictPlot, label ="predicted test")
+    ax.legend(loc="lower right")
+    ax.set_ylabel("Sea level (cm)")
+    ax.set_xlabel("period")
